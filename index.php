7f822236502cce6d434fe8db7fa83c2dee534038
Maxitano1 <dinhhieu12012002@gmail.com>
2025-05-11 14:30:42 +0700

    hieu

--- index.php
+++ index.php
@@ -3,17 +3,24 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JobHive</title>
 </head>
+
 <body>
     <?php
     include 'homepage/header.php';
     include 'homepage/body.php';
+
+    ?>
+</body>
+<footer>
+    <?php
     include 'homepage/footer.php';
     ?>
-    
-</body>
+</footer>
+
 </html>
