121756c1085ebdacde7b8cbd51abe9156ea3ec13
hnacud <163546210+ducanhap19@users.noreply.github.com>
2025-05-21 10:30:40 +0700

    Update header.php

--- homepage/header.php
+++ homepage/header.php
@@ -26,10 +26,33 @@
         <a href="#" style="text-decoration: none; color: #333;">Công cụ</a>
         <a href="#" style="text-decoration: none; color: #333;">Cẩm nang nghề nghiệp</a>
     </div>
+<<<<<<< Updated upstream
     <div style="display: flex; gap: 15px;">
         <a href="#" style="text-decoration: none; color: #007bff;">Đăng nhập</a>
         <a href="#" style="text-decoration: none; color: #007bff;">Đăng ký</a>
         <a href="#" style="text-decoration: none; color: #007bff;">Đăng tuyển & tìm hồ sơ</a>
+=======
+    <div style="display: flex; gap: 15px; margin-left: auto; margin-right: 50px;">
+        <?php if (isset($_SESSION['username'])): ?>
+            <div class="user-dropdown">
+                <div class="user-info">
+                    <img src="image/defaultavatar.jpg" alt="User Avatar">
+                    <span><?php echo htmlspecialchars($_SESSION['username']); ?></span>
+                </div>
+                <div class="dropdown-menu">
+                    <a href="jobseekerprofile.php">Profile</a>
+                    <a href="jobseekerlogout.php">Logout</a>
+                </div>
+            </div>
+
+
+        <?php else: ?>
+            <div class="guest-options">
+                <a href="jobseekerlogin.php" class="btn-login">Login/Register</a>
+                <a href="register.php" class="btn-register">Are you an employer? <br><span>Click here to redirect</span></a>
+            </div>
+        <?php endif; ?>
+>>>>>>> Stashed changes
     </div>
 </div>
 <h1>Welcome to JobHive</h1>
