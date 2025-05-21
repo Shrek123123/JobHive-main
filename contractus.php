62c246b1eb00bde36502e12bcdfd2f47ec8969e6
hnacud <163546210+ducanhap19@users.noreply.github.com>
2025-05-21 19:09:13 +0700

    aa

--- contractus.php
+++ contractus.php
@@ -103,7 +103,6 @@
     
     <div class="map-container">
         <h3>Find Us on Google Maps</h3>
-        <!-- Google Maps embed -->
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.230535335151!2d106.69840871485061!3d10.76315796223071!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752c2a7b44b28b%3A0xc0c0c0c0c0c0c0c0!2zU2hvcCBNYXJrZXQgS3V5QW5oYQ!5e0!3m2!1svi!2s!4v1612506615721!5m2!1svi!2s" 
         allowfullscreen="" loading="lazy"></iframe>
     </div>
@@ -114,3 +113,4 @@
     ?>
 </body>
 </html>
+<!--  -->
